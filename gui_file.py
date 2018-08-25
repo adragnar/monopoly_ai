62f74cd5eb4b8d5665260a8657ad23d6b02d82c1
Robert Adragna <robert.adragna1381@gmail.com>
2018-08-25 17:16:15 -0400

    Got the initial GUI up and running

---
+++ gui_file.py
@@ -0,0 +1,20 @@
+import PyQt5.QtWidgets
+
+import sys
+import gui_window_src
+
+
+class MainWindow(PyQt5.QtWidgets.QMainWindow,  gui_window_src.Ui_MainWindow) :
+
+    def __init__(self, parent=None):
+        super(PyQt5.QtWidgets.QMainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+        #Now start with real connections
+
+
+if __name__ == '__main__' :
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    form = MainWindow()
+    form.show()
+    app.exec()
