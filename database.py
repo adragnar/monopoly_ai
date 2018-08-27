79e83d52a69fc993660f0c436a8cfb443f9bc3d9
leafs1 <michael.adragna1@gmail.com>
2018-08-27 11:52:45 -0400

    Database updates. Working 'read' function that returns specified information. Working 'write' function that updates/edits existing information in the database.

--- database.py
+++ database.py
@@ -20,7 +20,6 @@ class database():
                       "price_for_three_houses", "price_for_four_houses", "price_for_one_hotel", "property_type",
                       "rent_for_one_railroad", "rent_for_two_railroads", "rent_for_three_railroads",
                       "rent_for_four_railroads", "board_position"]:
-            print(True)
             final = self.db.query("SELECT " + column + " FROM main_property_deck WHERE name = :row", row=row)
         elif column in ["player_name", "spot_on_board", "money", "num_get_out_of_jail", "is_in_jail", "num_of_doubles"]:
             final = self.db.query("SELECT " + column + " FROM player_information WHERE player_name = :row", row=row)
@@ -38,4 +37,31 @@ class database():
         return result
 
 
-    #def write_value(self, row, column, value):
+    def write_value(self, column, value, row):
+        """replaces/updates specific information in the database
+                Inputs: Row is string that is the new value. Column is a string name of type of info
+                we want to replace
+                Output: none
+                Class_vars_modified: none
+                """
+        if column in ["name", "owner", "is_available_for_purchase", "price", "real_estate_price", "rent",
+                      "num_of_houses", "is_mortgaged", "price_for_one_house", "price_for_two_houses",
+                      "price_for_three_houses", "price_for_four_houses", "price_for_one_hotel", "property_type",
+                      "rent_for_one_railroad", "rent_for_two_railroads", "rent_for_three_railroads",
+                      "rent_for_four_railroads", "board_position"]:
+            self.db.query("UPDATE main_property_deck SET " + column + "= " + value + " WHERE name = :row", row=row)
+        elif column in ["player_name", "spot_on_board", "money", "num_get_out_of_jail", "is_in_jail", "num_of_doubles"]:
+            self.db.query("UPDATE player_information SET " + column + "= " + value + " WHERE player_name = :row", row=row)
+        elif column in ["chance_name"]:
+            self.db.query("UPDATE chance_cards SET " + column + "= " + value + " WHERE chance_name = :row", row=row)
+        elif column in ["community_chest_name"]:
+            self.db.query("UPDATE community_chest_cards SET " + column + "= " + value + " WHERE community_chest_name = :row", row=row)
+        else:
+            # if the column doesn't exist anywhere (ie. a typo) return none
+            return None
+
+if __name__ == "__main__":
+    a = database()
+    print(a.write_value("money", "1500", "Player 2"))
+
+
